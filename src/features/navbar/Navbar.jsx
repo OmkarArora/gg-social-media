0e235e37afe0f4dee8f4fbebd467c48fadbc80d0
Omkar Arora <aroraomkar@gmail.com>
2021-06-09 14:14:26 +0530

    style: nav icon fill

--- src/features/navbar/Navbar.jsx
+++ src/features/navbar/Navbar.jsx
@@ -4,19 +4,19 @@ import {
   RiSearchLine,
   RiSearchFill,
   RiNotification3Fill,
-  RiNotification3Line
+  RiNotification3Line,
 } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
 export const Navbar = () => {
   // const { activeNavLink } = useNav();
-  const activeNavLink = "home"
+  const activeNavLink = "home";
   return (
     <div className="navbar-mobile">
       <Link to="/">
         <div className="btn-nav">
-          <div className="icon">
+          <div className={activeNavLink === "home" ? "icon icon-fill" : "icon"}>
             {activeNavLink === "home" ? <RiHome5Fill /> : <RiHome5Line />}
           </div>
           {/* <div className="heading">Home</div> */}
@@ -25,23 +25,29 @@ export const Navbar = () => {
 
       <Link to="/search">
         <div className="btn-nav">
-          <div className="icon">
+          <div
+            className={activeNavLink === "search" ? "icon icon-fill" : "icon"}
+          >
             {activeNavLink === "search" ? <RiSearchFill /> : <RiSearchLine />}
           </div>
-          {/* <div className="heading">Games</div> */}
         </div>
       </Link>
 
       <Link to="/notifications">
         <div className="btn-nav">
-          <div className="icon">
-            {activeNavLink === "notifications" ? <RiNotification3Fill /> : <RiNotification3Line />}
+          <div
+            className={
+              activeNavLink === "notifications" ? "icon icon-fill" : "icon"
+            }
+          >
+            {activeNavLink === "notifications" ? (
+              <RiNotification3Fill />
+            ) : (
+              <RiNotification3Line />
+            )}
           </div>
-          {/* <div className="heading">Guides</div> */}
         </div>
       </Link>
-
-      
     </div>
   );
 };
