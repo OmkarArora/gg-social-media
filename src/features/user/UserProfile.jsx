802e412d6c14a773e5763ea7c8d1f39a5e1b1c36
Omkar Arora <aroraomkar@gmail.com>
2021-06-18 12:06:14 +0530

    feat: User profile page components

--- src/features/user/UserProfile.jsx
+++ src/features/user/UserProfile.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { fetchUserFromUsername } from "./userSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "./userSlice";
+import { Avatar } from "shoto-ui";
 import "./userProfile.css";
 
 export const UserProfile = () => {
@@ -17,10 +18,39 @@ export const UserProfile = () => {
     if (!state) {
       dispatch(fetchUserFromUsername(username));
     } else {
-      if (state.user) dispatch(setUser({user: state.user}));
+      if (state.user) dispatch(setUser({ user: state.user }));
       else dispatch(fetchUserFromUsername(username));
     }
   }, [dispatch, state, username]);
 
-  return <div>user profile</div>;
+  return (
+    <div className="container-userProfile">
+      {user && (
+        <img src={user.bannerImage} alt="user banner" className="img-banner" />
+      )}
+      <div className="container-userInfo">
+        <div className="container-avatar">
+          <Avatar
+            alt={user?.name}
+            src={user?.profileImage}
+            height="5rem"
+            width="5rem"
+          />
+        </div>
+        <div>{user?.name}</div>
+        <div>@{user?.username}</div>
+        <div>
+          <div>{user?.location}</div>
+          <div>{user?.birthDate}</div>
+        </div>
+        <div>
+          <div>{user?.following} Following</div>
+          <div>{user?.followers} Followers</div>
+        </div>
+        <div>
+          <h3>Posts</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
