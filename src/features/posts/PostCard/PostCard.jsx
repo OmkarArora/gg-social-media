efc2f678e309314db6ac1590f918f3f7548b742a
Omkar Arora <aroraomkar@gmail.com>
2021-06-21 19:10:57 +0530

    feat: added username and time on postcard

--- src/features/posts/PostCard/PostCard.jsx
+++ src/features/posts/PostCard/PostCard.jsx
@@ -1,19 +1,29 @@
 import "./postcard.css";
 import { Avatar } from "shoto-ui";
+import { getRelativeTime } from "../../../helper";
 
 export const PostCard = ({ post }) => {
   return (
     <div className="container-postcard">
       <div className="container-profilePic">
         <Avatar
-          src={post.author.profileImage?post.author.profileImage:""}
+          src={post.author.profileImage ? post.author.profileImage : ""}
           alt={`${post.author.name}`}
           height="3rem"
           width="3rem"
         />
       </div>
       <div className="container-post-content">
-        <div className="author-name">{post.author.name}</div>
+        <div className="author-details">
+          <div>{post.author.name}</div>
+          <div className="text-grey">
+            @{post.author.username} <span className="separator-dot">.</span>{" "}
+            {getRelativeTime(
+              new Date().getTime(),
+              new Date(post.updatedAt).getTime()
+            )}
+          </div>
+        </div>
         <div className="post-content">{post.content.text}</div>
       </div>
     </div>
--- src/helper.js
+++ src/helper.js
@@ -6,3 +6,33 @@ export function setupAuthHeaderForServiceCalls(token) {
   }
   delete axios.defaults.headers.common["Authorization"];
 }
+
+export function getRelativeTime(current, previous) {
+  var msPerMinute = 60 * 1000;
+  var msPerHour = msPerMinute * 60;
+  var msPerDay = msPerHour * 24;
+  var msPerMonth = msPerDay * 30;
+  // var msPerYear = msPerDay * 365;
+
+  var elapsed = current - previous;
+
+  if (elapsed < msPerMinute) {
+    return Math.round(elapsed / 1000) + "s";
+  } else if (elapsed < msPerHour) {
+    return Math.round(elapsed / msPerMinute) + "m";
+  } else if (elapsed < msPerDay) {
+    return Math.round(elapsed / msPerHour) + "h";
+  } else if (elapsed < msPerMonth) {
+    if (elapsed / msPerDay <= 6)
+      return Math.round(elapsed / msPerDay) + "d";
+    else return "" + previous.getDate() + previous.getMonth();
+  }
+  return "" + previous.getDate() + previous.getMonth();
+  // else if (elapsed < msPerYear) {
+  //     return Math.round(elapsed/msPerMonth) + ' months ago';
+  // }
+
+  // else {
+  //     return Math.round(elapsed/msPerYear ) + ' years ago';
+  // }
+}
