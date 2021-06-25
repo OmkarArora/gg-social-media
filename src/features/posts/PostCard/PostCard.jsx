5fb0b46a443be996ac25e559b39cdf1149fea613
Omkar Arora <aroraomkar@gmail.com>
2021-06-25 20:28:11 +0530

    feat: Link to user from postcard

--- src/features/posts/PostCard/PostCard.jsx
+++ src/features/posts/PostCard/PostCard.jsx
@@ -1,17 +1,20 @@
-import "./postcard.css";
 import { Avatar } from "shoto-ui";
 import { getRelativeTime } from "../../../helper";
+import { Link } from "react-router-dom";
+import "./postcard.css";
 
 export const PostCard = ({ post }) => {
   return (
     <div className="container-postcard">
       <div className="container-profilePic">
-        <Avatar
-          src={post.author.profileImage ? post.author.profileImage : ""}
-          alt={`${post.author.name}`}
-          height="3rem"
-          width="3rem"
-        />
+        <Link to={`/${post.author.username}`}>
+          <Avatar
+            src={post.author.profileImage ? post.author.profileImage : ""}
+            alt={`${post.author.name}`}
+            height="3rem"
+            width="3rem"
+          />
+        </Link>
       </div>
       <div className="container-post-content">
         <div className="author-details">
