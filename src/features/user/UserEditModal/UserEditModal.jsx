89514a4e42e378e4eb6ab57bcb49e92f00c98f19
Omkar Arora <aroraomkar@gmail.com>
2021-08-07 13:45:22 +0530

    feat: user banner uploader

--- src/features/user/UserEditModal/UserEditModal.jsx
+++ src/features/user/UserEditModal/UserEditModal.jsx
@@ -1,11 +1,15 @@
-import { Button } from "shoto-ui";
+import { useState, useEffect } from "react";
+import { Button, Avatar } from "shoto-ui";
 import { RiCloseLine } from "react-icons/ri";
-import { Avatar } from "shoto-ui";
-import "./userEditModal.css";
 import { ControlledInput } from "../ControlledInput/ControlledInput";
-import { useState } from "react";
 import { updateUserDetails } from "../../authentication/authSlice";
+import { showAlert } from "../../alert/alertSlice";
 import { useDispatch } from "react-redux";
+import { ImageUploader } from "../../posts/NewPostModal/ImageUploader";
+import axios from "axios";
+import "./userEditModal.css";
+import { crop } from "../../../helper";
+import { LoadingModal } from "../../loader/LoadingModal/LoadingModal";
 
 export const UserEditModal = ({ setEditProfileModal, user, profileImage }) => {
   const [name, setName] = useState(user.name);
@@ -15,6 +19,54 @@ export const UserEditModal = ({ setEditProfileModal, user, profileImage }) => {
   const [birthDate, setBirthDate] = useState(
     user.birthDate ? user.birthDate : null
   );
+
+  const [localAppState, setLocalAppState] = useState("success");
+
+  // Image uploader
+  const [previewImage, setPreviewImage] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadedURL, setUploadedURL] = useState("");
+  const [croppedBanner, setCroppedBanner] = useState(null);
+  const [bannerBlob, setBannerBlob] = useState(null);
+
+  useEffect(() => {
+    if (previewImage !== null) {
+      (async () => {
+        const croppedCanvas = await crop(previewImage, 1500 / 500);
+        const croppedImage = croppedCanvas.toDataURL();
+        setCroppedBanner(croppedImage);
+        croppedCanvas.toBlob((blob) => {
+          setBannerBlob(blob);
+        }, "image/png");
+      })();
+    }
+  }, [previewImage]);
+
+  const uploadImage = async (imageFile) => {
+    let axiosInstance = axios.create();
+    delete axiosInstance.defaults.headers.common["Authorization"];
+
+    try {
+      const formData = new FormData();
+      formData.append("file", imageFile);
+      formData.append(
+        "upload_preset",
+        `${process.env.REACT_APP_IMAGE_UPLOAD_PRESET}`
+      );
+      formData.append("folder", "firstFolder-1");
+      const response = await axiosInstance.post(
+        `${process.env.REACT_APP_IMAGE_UPLOAD_API}`,
+        formData
+      );
+      setUploadedURL(response.data.secure_url);
+      setPreviewImage(null);
+      setSelectedFile(null);
+      return response.data.secure_url;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
 
   const dispatch = useDispatch();
 
@@ -33,16 +85,31 @@ export const UserEditModal = ({ setEditProfileModal, user, profileImage }) => {
     "Dec",
   ];
 
-  const saveUserDetails = () => {
+  const saveUserDetails = async () => {
+    setLocalAppState("loading");
+    let imageURL = "";
+    try {
+      if (bannerBlob) {
+        imageURL = await uploadImage(bannerBlob);
+      }
+    } catch (error) {
+      dispatch(showAlert({ type: "error", data: "Couldn't upload image" }));
+      return;
+    }
+
     let updates = {};
     updates.name = name;
     updates.bio = bio;
     updates.location = location;
     updates.website = website;
-    if(birthDate){
+    if (birthDate) {
       updates.birthDate = new Date(birthDate).toISOString();
     }
-    dispatch(updateUserDetails({userUpdates: updates, userId: user._id}));
+    if (croppedBanner) {
+      updates.bannerImage = imageURL;
+    }
+    dispatch(updateUserDetails({ userUpdates: updates, userId: user._id }));
+    setLocalAppState("success");
     setEditProfileModal(false);
   };
 
@@ -61,7 +128,7 @@ export const UserEditModal = ({ setEditProfileModal, user, profileImage }) => {
     date.setDate(date.getDate());
     date = date.toISOString().substr(0, 10);
     return date;
-  }
+  };
 
   return (
     <div className="container-user-editModal">
@@ -84,7 +151,13 @@ export const UserEditModal = ({ setEditProfileModal, user, profileImage }) => {
         </div>
         <div className="editModal-content">
           <div className="container-banner">
-            {user.bannerImage ? (
+            {croppedBanner ? (
+              <img
+                src={croppedBanner}
+                alt="user banner"
+                className="img-banner"
+              />
+            ) : user.bannerImage ? (
               <img
                 src={user.bannerImage}
                 alt="user banner"
@@ -93,6 +166,16 @@ export const UserEditModal = ({ setEditProfileModal, user, profileImage }) => {
             ) : (
               <div className="banner-placeholder">{user.name}</div>
             )}
+            <span className="container-bannerImageUploader">
+              <ImageUploader
+                previewImage={previewImage}
+                setPreviewImage={setPreviewImage}
+                selectedFile={selectedFile}
+                setSelectedFile={setSelectedFile}
+                uploadedURL={uploadedURL}
+                setUploadedURL={setUploadedURL}
+              />
+            </span>
           </div>
           <div className="container-userInfo">
             <div className="container-avatar">
@@ -137,9 +220,9 @@ export const UserEditModal = ({ setEditProfileModal, user, profileImage }) => {
                 <div className="header-birthDate">Birth date</div>
                 <div className="container-dateEdit">
                   {birthDate &&
-                    `${
-                      monthNames[new Date(birthDate).getMonth()]
-                    } ${new Date(birthDate).getDate()}, ${new Date(birthDate).getFullYear()}`}
+                    `${monthNames[new Date(birthDate).getMonth()]} ${new Date(
+                      birthDate
+                    ).getDate()}, ${new Date(birthDate).getFullYear()}`}
                   <input
                     type="date"
                     max={getCurrentDate()}
@@ -154,6 +237,7 @@ export const UserEditModal = ({ setEditProfileModal, user, profileImage }) => {
           </div>
         </div>
       </div>
+      {localAppState === "loading" && <LoadingModal />}
     </div>
   );
 };
